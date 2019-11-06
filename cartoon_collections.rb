0523a995455205e47f1fbb7bde7491eec50b0f2a
Brian Nicholls <nichol88@gmail.com>
2019-11-06 19:13:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(array)# code an argument here
   }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  result = array.collect { |n|
+    p "#{n.capitalize}!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
